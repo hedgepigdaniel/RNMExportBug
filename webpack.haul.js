cdca224db6d24316ced001c72f906c59be8153e9
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-01-17 09:33:27 +1100

    Apply a second babel-loader to react-native-maps to transpile it to commonjs modules and avoid the error

--- webpack.haul.js
+++ webpack.haul.js
@@ -8,5 +8,25 @@ module.exports = ({ platform }, defaults) => ({
     // => WARNING: export <not_exported_name> was not found in './file.js'
     // I think this is a warning by default but haul hides warnings by default and
     // this makes it an "error" (which looks like a warning)
+
+    rules: [
+      ...defaults.module.rules,
+      {
+        test: /node_modules\/react-native-maps\/.*.js/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ['env', {
+                // This is the default, but preempts the other babel-loader using .babelrc
+                // This is because react-native-maps has es6 modules with
+                // `module.exports = ...;` at the end of each file instead of `export default ...;`
+                modules: 'commonjs',
+              }]
+            ],
+          },
+        },
+      },
+    ],
   },
 });
