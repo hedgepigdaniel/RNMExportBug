02013b780a8085b8923afdd0b822acc990d13588
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-01-17 09:31:02 +1100

    Configure babel to not transpile es6 modules to commonjs modules, so that webpack can warn on undefined imports

--- webpack.haul.js
+++ webpack.haul.js
@@ -1,3 +1,12 @@
-module.exports = ({ platform }) => ({
+module.exports = ({ platform }, defaults) => ({
   entry: `./index.js`,
+  module: {
+    ...defaults.module,
+
+    // import { <not_exported_name> } from './file.js';
+    strictExportPresence: true,
+    // => WARNING: export <not_exported_name> was not found in './file.js'
+    // I think this is a warning by default but haul hides warnings by default and
+    // this makes it an "error" (which looks like a warning)
+  },
 });
